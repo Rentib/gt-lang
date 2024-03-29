7c5612f1feef7d6d906a158159dd2c3616575ad8
Rentib <sbitner420@tutanota.com>
2024-03-29 23:42:29 +0100

    feat(Exceptions): basic exceptions

--- src/Common/Exceptions.hs
+++ src/Common/Exceptions.hs
@@ -1,1 +1,19 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+
 module Common.Exceptions where
+
+import Parser.Abs
+
+type GTException = GTException' BNFC'Position
+data GTException' a where
+    NotImplementedGTException :: a -> GTException' a
+    UnknownRuntimeGTException :: a -> GTException' a
+
+instance Show GTException where
+    show (NotImplementedGTException pos) = "Not implemented at " ++ showpos pos
+    show (UnknownRuntimeGTException pos) = "Unknown runtime exception at " ++ showpos pos
+
+showpos :: BNFC'Position -> String
+showpos (Just (l, c)) = "line " ++ show l ++ ", column " ++ show c
+showpos Nothing = "unknown position"
